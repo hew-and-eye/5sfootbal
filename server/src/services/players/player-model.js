226309cfaa328ed1ddb3d847da92c098104cc194
Till Bauer <bauertill@gmail.com>
2018-10-21 09:42:11 +0200

    Add player model

---
+++ server/src/services/players/player-model.js
@@ -0,0 +1,33 @@
+'use strict';
+
+// answer-model.js - A mongoose model
+// 
+// See http://mongoosejs.com/docs/models.html
+// for more of what you can do here.
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const {ObjectId} = Schema.Types
+
+// all coordinates are in feet with vertical orientation. top left back of the endzone is (0,0)
+
+
+export const playerSchema = new Schema({
+  name: String,
+  stats: {
+    size: Number,
+    strength: Number,
+    speed: Number,
+    skill: Number,
+    smart: Number
+  },
+  jerseyNumber: Number,
+  teamId: ObjectId
+});
+
+
+
+
+const playerModel = mongoose.model('player', playerSchema);
+
+export default playerModel;
