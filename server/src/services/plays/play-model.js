c07ea6f3946c35f42a156dbf1eb397f9e939384c
Till Bauer <bauertill@gmail.com>
2018-10-22 00:37:30 +0200

    Refactor dummy data to match

--- server/src/services/plays/play-model.js
+++ server/src/services/plays/play-model.js
@@ -15,65 +15,40 @@ const coordinateSchema = new Schema({
   y: Number
 })
 
-export const playSchema = new Schema({
-  play: [
-    {
-      team: { Type: { String, enum: ['Offense', 'Defense'] } },
-      teamName: String,
-      teamId: ObjectId,
-      player: playerSchema,
-      coordinates: coordinateSchema,
-      coordinatesMovie: [
-        coordinateSchema
-      ],
-      physics: {
-        d: coordinateSchema,
-        v: coordinateSchema,
-        a: coordinateSchema,
-        m: Number,
-        vMax: Number
-      },
-      actions: [
-        {
-          actionType: { Type: String, enum: ['runBlock', 'run', 'handOff', 'manCoverage', 'zoneCoverage', 'blitz'] },
-          params: {
-            opposingPlayer: Number,
-            direction: coordinateSchema,
-            runPoints: [coordinateSchema],
-            playerFrom: Number,
-            playerTo: Number,
-            point: coordinateSchema,
-            radius: Number
-          },
-          duration: Number
-        }
-      ]
-    }
-  ],
-  defensivePlay: [
-    {
-      player: playerSchema,
-      coordinates: {
-        x: Number,
-        y: Number
-      },
-      actions: [
-        {
-          actionType: { Type: String, enum: ['manCoverage', 'zoneCoverage', 'blitz'] },
-          params: {
-            opposingPlayer: Number, //index in players array, 0-4 linemen, 5-9 skillposition, 10qb
-            point: {
-              x: Number,
-              y: Number
-            },
-            radius: Number
-          },
-          duration: Number
-        }
-      ]
-    }
-  ],
-});
+export const playSchema = new Schema([
+  {
+    team: { Type: { String, enum: ['Offense', 'Defense'] } },
+    teamName: String,
+    teamId: ObjectId,
+    player: playerSchema,
+    coordinates: coordinateSchema,
+    coordinatesMovie: [
+      coordinateSchema
+    ],
+    physics: {
+      d: coordinateSchema,
+      v: coordinateSchema,
+      a: coordinateSchema,
+      m: Number,
+      vMax: Number
+    },
+    actions: [
+      {
+        actionType: { Type: String, enum: ['runBlock', 'run', 'handOff', 'manCoverage', 'zoneCoverage', 'blitz'] },
+        params: {
+          opposingPlayer: Number,
+          direction: coordinateSchema,
+          runPoints: [coordinateSchema],
+          playerFrom: Number,
+          playerTo: Number,
+          point: coordinateSchema,
+          radius: Number
+        },
+        duration: Number
+      }
+    ]
+  }
+]);
 
 
 const play = mongoose.model('play', playSchema);
